f96b89e81669ec47ff7bba69e11c1885c6a36674
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2023-12-02 20:30:56 +0300

    Update alg_dict.py

--- easyeditor/util/alg_dict.py
+++ easyeditor/util/alg_dict.py
@@ -1,14 +1,14 @@
 from ..models.rome import ROMEHyperParams, apply_rome_to_model
 from ..models.memit import MEMITHyperParams, apply_memit_to_model
 from ..models.kn import KNHyperParams, apply_kn_to_model
-from ..models.mend import MENDHyperParams, MendRewriteExecutor, MendMultimodalRewriteExecutor
+from ..models.mend import MENDHyperParams, MendRewriteExecutor
 from ..models.ft import FTHyperParams, apply_ft_to_model
-from ..models.serac import SERACHparams, SeracRewriteExecutor, SeracMultimodalRewriteExecutor
-from ..dataset import ZsreDataset, CounterFactDataset, CaptionDataset, VQADataset
-from ..models.ike import IKEHyperParams, apply_ike_to_model, apply_ike_to_multimodal_model
+from ..models.serac import SERACHparams, SeracRewriteExecutor
+from ..dataset import ZsreDataset, CounterFactDataset
+from ..models.ike import IKEHyperParams, apply_ike_to_model
 from ..models.ft_api import FTApiHyperParams, apply_ft_api_to_model
 from ..models.lora import LoRAHyperParams, apply_lora_to_model
-
+from ..models.grace import GraceHyperParams, apply_grace_to_model
 
 ALG_DICT = {
     'ROME': apply_rome_to_model,
@@ -20,18 +20,11 @@ ALG_DICT = {
     'IKE': apply_ike_to_model,
     'FT-Api': apply_ft_api_to_model,
     'LoRA': apply_lora_to_model,
+    'GRACE': apply_grace_to_model,
 }
 
-ALG_MULTIMODAL_DICT = {
-    'MEND': MendMultimodalRewriteExecutor().apply_to_model,
-    'SERAC': SeracMultimodalRewriteExecutor().apply_to_model,
-    'SERAC_MULTI': SeracMultimodalRewriteExecutor().apply_to_model,
-    'IKE': apply_ike_to_multimodal_model,
-}
 
 DS_DICT = {
     "cf": CounterFactDataset,
-    "zsre": ZsreDataset,
-    "caption": CaptionDataset,
-    "vqa": VQADataset
+    "zsre": ZsreDataset
 }
