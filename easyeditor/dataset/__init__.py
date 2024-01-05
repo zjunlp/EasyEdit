be950d8792cf7eac5c99ba6fbc586ccfae09c873
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2024-01-05 18:47:16 +0800

    Update __init__.py

--- easyeditor/dataset/__init__.py
+++ easyeditor/dataset/__init__.py
@@ -3,3 +3,4 @@ from .zsre import ZsreDataset
 from .coco_caption import CaptionDataset
 from .vqa import VQADataset
 from .wiki_recent import WikiRecentDataset
+from .knowedit import KnoweditDataset
