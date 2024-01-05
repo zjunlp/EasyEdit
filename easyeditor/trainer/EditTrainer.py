7fe869ead2b15c58d261e2fbf4ae6077273553f8
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2024-01-05 18:42:36 +0800

    Update EditTrainer.py

--- easyeditor/trainer/EditTrainer.py
+++ easyeditor/trainer/EditTrainer.py
@@ -42,7 +42,10 @@ class EditTrainer(BaseTrainer):
 
         # Do the edit
         start = time.time()
-        edited_model, model_info = self.model.edit(batch["edit_inner"], batch["cond"])
+        if "cond" in batch:
+            edited_model, model_info = self.model.edit(batch["edit_inner"], batch["cond"])
+        else:
+            edited_model, model_info = self.model.edit(batch["edit_inner"])
         edit_time = time.time() - start
 
         with torch.set_grad_enabled(training):
@@ -66,8 +69,7 @@ class EditTrainer(BaseTrainer):
 
         if training:
             safe_backward(
-                l_total_edit, self.model.outer_parameters(), self.config.accumulate_bs, allow_unused=True if
-                self.config.alg=='MEND' and self.config.model_parallel else False
+                l_total_edit, self.model.outer_parameters(), self.config.accumulate_bs
             )
 
         # Collect some useful metrics
