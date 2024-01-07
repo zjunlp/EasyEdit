c07a485ff48c51dca3a7c738af28ad1af8afbac4
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2024-01-07 23:17:08 +0800

    Create run_knowedit_llama2.py

---
+++ examples/run_knowedit_llama2.py
@@ -0,0 +1,254 @@
+import os.path
+import sys
+import json
+import random
+from easyeditor import (
+    FTHyperParams, 
+    IKEHyperParams, 
+    KNHyperParams, 
+    MEMITHyperParams, 
+    ROMEHyperParams, 
+    LoRAHyperParams,
+    MENDHyperParams,
+    SERACHparams
+    )
+from easyeditor import BaseEditor
+from easyeditor.models.ike import encode_ike_facts
+from sentence_transformers import SentenceTransformer
+from easyeditor import KnoweditDataset
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--editing_method', required=True, type=str)
+    parser.add_argument('--hparams_dir', required=True, type=str)
+    parser.add_argument('--data_dir', required=True, type=str)
+    parser.add_argument('--ds_size', default=None, type=int)
+    parser.add_argument('--metrics_save_dir', default='./output', type=str)
+    parser.add_argument('--datatype',default=None,type=str)
+
+    args = parser.parse_args()
+
+    if args.editing_method == 'FT':
+        editing_hparams = FTHyperParams
+    elif args.editing_method == 'IKE':
+        editing_hparams = IKEHyperParams
+    elif args.editing_method == 'KN':
+        editing_hparams = KNHyperParams
+    elif args.editing_method == 'MEMIT':
+        editing_hparams = MEMITHyperParams
+    elif args.editing_method == 'ROME':
+        editing_hparams = ROMEHyperParams
+    elif args.editing_method == 'LoRA':
+        editing_hparams = LoRAHyperParams
+    else:
+        raise NotImplementedError
+    
+
+    datas = KnoweditDataset(args.data_dir)
+    if args.ds_size is not None:
+        datas = random.sample(datas, args.ds_size)
+
+    if args.datatype == 'counterfact':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[datas['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        portability_r =[data['portability_r'] for data in datas]
+        portability_s =[data['portability_s'] for data in datas]
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+
+        portability_reasoning_prompts=[]
+        portability_reasoning_ans=[]
+        for pr in portability_r:
+            prompt=pr["prompt"]
+            an=pr["ground_truth"][0][0]
+            portability_reasoning_prompts.append(prompt)
+            portability_reasoning_ans.append(an)
+    
+        portability_Subject_Aliasing_prompts=[]
+        portability_Subject_Aliasing_ans=[]           
+        for ps in portability_s:
+            prompt=ps["prompt"]
+            an=ps["ground_truth"][0][0]
+            portability_Subject_Aliasing_prompts.append(prompt)
+            portability_Subject_Aliasing_ans.append(an)
+
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        for lr in locality_rs:
+            prompt=lr["prompt"]
+            an=lr["ground_truth"][0][0]
+            locality_Relation_Specificity_prompts.append(prompt)
+            locality_Relation_Specificity_ans.append(an)
+
+        locality_Forgetfulness_prompts=[]        
+        locality_Forgetfulness_ans=[]
+        for lf in locality_f:
+            prompt = lf["prompt"]
+            an = lf["ground_truth"][0][0]
+            locality_Forgetfulness_prompts.append(prompt)
+            locality_Forgetfulness_ans.append(an)
+
+    if args.datatype == 'recent':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[datas['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        portability_r =[data['portability_r'] for data in datas]
+        portability_s =[data['portability_s'] for data in datas]
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+
+        portability_reasoning_prompts=[]
+        portability_reasoning_ans=[]
+        for pr in portability_r:
+            prompt=pr["prompt"]
+            an=pr["ground_truth"][0][0]
+            portability_reasoning_prompts.append(prompt)
+            portability_reasoning_ans.append(an)
+    
+        portability_Subject_Aliasing_prompts=[]
+        portability_Subject_Aliasing_ans=[]           
+        for ps in portability_s:
+            prompt=ps["prompt"]
+            an=ps["ground_truth"][0][0]
+            portability_Subject_Aliasing_prompts.append(prompt)
+            portability_Subject_Aliasing_ans.append(an)
+
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        for lr in locality_rs:
+            prompt=lr["prompt"]
+            an=lr["ground_truth"][0][0]
+            locality_Relation_Specificity_prompts.append(prompt)
+            locality_Relation_Specificity_ans.append(an)
+
+        locality_Forgetfulness_prompts=[]        
+        locality_Forgetfulness_ans=[]
+        for lf in locality_f:
+            prompt = lf["prompt"]
+            an = lf["ground_truth"][0][0]
+            locality_Forgetfulness_prompts.append(prompt)
+            locality_Forgetfulness_ans.append(an)
+
+    if args.datatype == 'wikibio':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[datas['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        portability_r =[data['portability_r'] for data in datas]
+        portability_s =[data['portability_s'] for data in datas]
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+
+        portability_reasoning_prompts=[]
+        portability_reasoning_ans=[]
+        for pr in portability_r:
+            prompt=pr["prompt"]
+            an=pr["ground_truth"][0]
+            portability_reasoning_prompts.append(prompt)
+            portability_reasoning_ans.append(an)
+    
+        portability_Subject_Aliasing_prompts=[]
+        portability_Subject_Aliasing_ans=[]           
+        for ps in portability_s:
+            prompt=ps["prompt"]
+            an=ps["ground_truth"][0]
+            portability_Subject_Aliasing_prompts.append(prompt)
+            portability_Subject_Aliasing_ans.append(an)
+
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        for lr in locality_rs:
+            prompt=lr["prompt"]
+            an=lr["ground_truth"][0]
+            locality_Relation_Specificity_prompts.append(prompt)
+            locality_Relation_Specificity_ans.append(an)
+
+        locality_Forgetfulness_prompts=[]        
+        locality_Forgetfulness_ans=[]
+        for lf in locality_f:
+            prompt = lf["prompt"]
+            an = lf["ground_truth"][0]
+            locality_Forgetfulness_prompts.append(prompt)
+            locality_Forgetfulness_ans.append(an)
+
+    if args.datatype == 'zsre':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[datas['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        portability_r =[data['portability_r'] for data in datas]
+        portability_s =[data['portability_s'] for data in datas]
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+
+        portability_reasoning_prompts=[]
+        portability_reasoning_ans=[]
+        for pr in portability_r:
+            prompt=pr["prompt"]
+            an=pr["ground_truth"][0]
+            portability_reasoning_prompts.append(prompt)
+            portability_reasoning_ans.append(an)
+    
+        portability_Subject_Aliasing_prompts=[]
+        portability_Subject_Aliasing_ans=[]           
+        for ps in portability_s:
+            prompt=ps["prompt"]
+            an=ps["ground_truth"][0]
+            portability_Subject_Aliasing_prompts.append(prompt)
+            portability_Subject_Aliasing_ans.append(an)
+
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        for lr in locality_rs:
+            prompt=lr["prompt"]
+            an=lr["ground_truth"][0]
+            locality_Relation_Specificity_prompts.append(prompt)
+            locality_Relation_Specificity_ans.append(an)
+
+        locality_Forgetfulness_prompts=[]        
+        locality_Forgetfulness_ans=[]
+        for lf in locality_f:
+            prompt = lf["prompt"]
+            an = lf["ground_truth"][0]
+            locality_Forgetfulness_prompts.append(prompt)
+            locality_Forgetfulness_ans.append(an)
+
+
+
+
+    locality_inputs = {
+        'Relation_Specificity':{
+            'prompt': locality_Relation_Specificity_prompts,
+            'ground_truth': locality_Relation_Specificity_ans
+        },
+        'Forgetfulness':{
+            'prompt':locality_Forgetfulness_prompts,
+            'ground_truth':locality_Forgetfulness_ans
+        }
+    }
+    portability_inputs = {
+        'Subject_Aliasing':{
+            'prompt': portability_Subject_Aliasing_prompts,
+            'ground_truth': portability_Subject_Aliasing_ans
+        },
+        'reasoning':{
+            'prompt': portability_reasoning_prompts,
+            'ground_truth': portability_reasoning_ans           
+        }
+    }
+
+    hparams = editing_hparams.from_hparams(args.hparams_dir)
+
+
+    editor = BaseEditor.from_hparams(hparams)
+    metrics, edited_model, _ = editor.edit(
+        prompts=prompts,
+        target_new=target_new,
+        subject=subjects,
+        locality_inputs=locality_inputs,
+        portability_inputs=portability_inputs,
+        keep_original_weight=True
+    )
+
+    json.dump(metrics, open(os.path.join(args.metrics_save_dir, f'{args.editing_method}_results.json'), 'w'), indent=4)
