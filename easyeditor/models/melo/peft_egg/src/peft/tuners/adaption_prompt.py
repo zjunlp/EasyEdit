bd286dcad0e93aff291919fed300aa969d7460e2
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2024-02-15 18:24:33 +0800

    Update adaption_prompt.py

--- easyeditor/models/melo/peft_egg/src/peft/tuners/adaption_prompt.py
+++ easyeditor/models/melo/peft_egg/src/peft/tuners/adaption_prompt.py
@@ -22,7 +22,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from peft.utils.config import PeftConfig, PeftType
+from ..utils.config import PeftConfig, PeftType
 from peft.utils.other import _freeze_adapter, _get_submodules
 
 
