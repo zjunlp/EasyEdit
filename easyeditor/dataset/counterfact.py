f993fca32ddc6f37dacb3f5fbfe5f7ddb918a85c
Zekun Xi <70566607+XeeKee@users.noreply.github.com>
2024-04-16 15:22:48 +0800

    Update counterfact.py

--- easyeditor/dataset/counterfact.py
+++ easyeditor/dataset/counterfact.py
@@ -47,6 +47,7 @@ class CounterFactDataset(Dataset):
 
         with open(cf_loc, "r") as f:
             self.data = json.load(f)
+            self.data = self.data[:3]
         if size is not None:
             self.data = self.data[:size]
 
@@ -140,15 +141,32 @@ class CounterFactDataset(Dataset):
 
 
     def collate_gpt_fn(self, batch):
-        src = [b["prompt"] for b in batch]
-        trg = [' ' + b["target_new" for b in batch]
-        cond = ["{} >> {} || {}".format(b['ground_truth'],
-                                        b["target_new"],
-                                        b['prompt']) for b in batch]
-        rephrase = [b["rephrase_prompt"] for b in batch]
-        loc = [b["locality_prompt"] for b in batch]
-        loc_ans = [' ' + b["locality_ground_truth"] for b in batch]
-
+        if self.config.alg =='SERAC' and 'gpt' in self.config.model_name.lower():
+            src = [b["prompt"] for b in batch]
+            trg = [' ' + b["target_new"] for b in batch]
+            cond = ["{} >> {} || {}".format(b['ground_truth'],
+                                            b["target_new"],
+                                            b['prompt']) for b in batch]
+            rephrase = [b["rephrase_prompt"] for b in batch]
+            loc = [b["locality_prompt"] for b in batch]
+            loc_ans = [' ' + b["locality_ground_truth"] for b in batch]
+            
+            src = [src_ + trg_ for src_, trg_ in zip(src, trg)]
+            rephrase = [rephrase_ + trg_ for rephrase_, trg_ in zip(rephrase, trg)]
+            loc = [loc_ + loc_ans_ for loc_, loc_ans_ in zip(loc, loc_ans)]
+        else:
+            src = [b["prompt"] for b in batch]
+            trg = [b["target_new"] for b in batch]
+            cond = ["{} >> {} || {}".format(b['ground_truth'],
+                                            b["target_new"],
+                                            b['prompt']) for b in batch]
+            rephrase = [b["rephrase_prompt"] for b in batch]
+            loc = [b["locality_prompt"] for b in batch]
+            loc_ans = [b["locality_ground_truth"] for b in batch]
+            
+            src = [src_ + ' ' + trg_ for src_, trg_ in zip(src, trg)]
+            rephrase = [rephrase_ + ' ' + trg_ for rephrase_, trg_ in zip(rephrase, trg)]
+            loc = [loc_ + ' ' + loc_ans_ for loc_, loc_ans_ in zip(loc, loc_ans)]
         # if (hasattr(self.config, 'alg') and self.config.alg == 'SERAC') or \
         #         (hasattr(self.config, 'alg_name') and self.config.alg_name == 'SERAC'):
         #     def flatten(nested_list: typing.List[typing.List]):
@@ -175,9 +193,7 @@ class CounterFactDataset(Dataset):
         #     src, rephrase, trg, loc, loc_ans = flatten(src), flatten(rephrase), flatten(trg), flatten(loc), flatten(loc_ans)
         #
         # else:
-        src = [src_ + trg_ for src_, trg_ in zip(src, trg)]
-        rephrase = [rephrase_ + trg_ for rephrase_, trg_ in zip(rephrase, trg)]
-        loc = [loc_ +  loc_ans_ for loc_, loc_ans_ in zip(loc, loc_ans)]
+
 
         batches = {
             f"{k1}_{k2}": v2
@@ -205,6 +221,8 @@ class CounterFactDataset(Dataset):
         edit_labels = self.get_edit_labels(batches["trg_input_ids"])
 
         edit_inner["labels"] = edit_labels
+        
+
 
         edit_rephrase = {}
         edit_rephrase["input_ids"] = batches["rephrase_input_ids"]
