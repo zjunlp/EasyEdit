247fdc82371a1224444c1f64b5b33d522a4a970a
JizhanFang <157343825+JizhanFang@users.noreply.github.com>
2024-09-17 10:11:01 +0800

    Update wise_main.py
    support WISE batch_edit

--- easyeditor/models/wise/wise_main.py
+++ easyeditor/models/wise/wise_main.py
@@ -13,17 +13,17 @@ def apply_wise_to_model(
         copy=False,
         **kwargs: Any,
 ) -> Tuple[AutoModelForCausalLM, Dict[str, Any]]:
-    request = requests[0]
     if copy:
         model = deepcopy(model)
     device = f'cuda:{hparams.device}'
     context_templates = get_context_templates(model, tok, length_params=[[5,5], [10,5]], device=device)
     editor = WISE(model=model, config=hparams, device=device)
-    print(
-        f"Executing WISE algorithm for the update: "
-        f"[{request['prompt']}] -> [{request['target_new']}]"
-    )
-    tokens, act_mask, deact_mask = tokenize(request, tokenizer=tok, device=device, context_templates=context_templates, hparams=hparams)
+    print(f"Executing WISE algorithm for the update: ")
+    for request in requests:
+        print(
+            f"[{request['prompt']}] -> [{request['target_new']}]"
+        )
+    tokens, act_mask, deact_mask = tokenize(requests, tokenizer=tok, device=device, context_templates=context_templates, hparams=hparams)
     editor.edit(config=hparams, tokens=tokens, act_mask=act_mask, deact_mask=deact_mask)
 
     weights_copy = editor.reset_layer
