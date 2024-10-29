97163b4b009513671e8732e3d70622283fc3e8e1
auto <v-lauraom@gpuhost09.cm.cluster>
2024-10-29 14:47:19 +0900

    add bash files

--- la_scripts/run_mend.py
+++ la_scripts/run_mend.py
@@ -1,10 +1,13 @@
-
+import sys, os
+# sys.path.append(f"{os.path.dirname(os.getcwd())}/easyeditor")
+sys.path.append("../")
+import easyeditor
 from easyeditor import EditTrainer, MENDTrainingHparams, ZsreDataset
 
 
-training_hparams = MENDTrainingHparams.from_hparams('hparams/TRAINING/MEND/llama-7b.yaml')
-train_ds = ZsreDataset('./data/zsre/zsre_mend_train.json', config=training_hparams)
-eval_ds = ZsreDataset('./data/zsre/zsre_mend_eval.json', config=training_hparams)
+training_hparams = MENDTrainingHparams.from_hparams('../hparams/TRAINING/MEND/llama-7b.yaml')
+train_ds = ZsreDataset('../data/zsre/zsre_mend_train.json', config=training_hparams)
+eval_ds = ZsreDataset('../data/zsre/zsre_mend_eval.json', config=training_hparams)
 trainer = EditTrainer(
     config=training_hparams,
     train_set=train_ds,
---
+++ la_scripts/run_mend.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --job-name=mend_zsre
+#SBATCH --out="log_test_mend0.txt"
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:tesla_a100:1
+#SBATCH --cpus-per-gpu=4
+
+conda init
+conda activate myenv397
+source /home/smg/v-lauraom/venvs/EasyEdit/bin/activate 
+
+python la_scripts/run_mend.py
