838c11a706bc93f3518cb10d8cdd71a31c62c59f
mengru wang <125349178+mengrusun@users.noreply.github.com>
2024-11-16 15:00:02 +0800

    Add files via upload

--- easyeditor/evaluate/safety_evaluate.py
+++ easyeditor/evaluate/safety_evaluate.py
@@ -47,21 +47,21 @@ def compute_safety_edit_quality(
     }
     return ret
 
-def ccks_compute_safety_edit_quality(
-    model,
-    # model_name,
-    # hparams: HyperParams,
-    tok: AutoTokenizer,
-    record: typing.Dict,
-    device,
-    # test_generation = False
-    max_tokens = 600,
-    max_output_tokens: int = 400,
-) -> typing.Dict:
-    batch = [record["prompt"]] + record['general_prompt']
-    DS, DG_otherAQ = test_safety_gen(model, tok, batch, device, max_tokens, max_output_tokens)
-    ret = {
-        "DS": DS,
-        "DG_otherAQ": DG_otherAQ
-    }
-    return ret
+# def ccks_compute_safety_edit_quality(
+#     model,
+#     # model_name,
+#     # hparams: HyperParams,
+#     tok: AutoTokenizer,
+#     record: typing.Dict,
+#     device,
+#     # test_generation = False
+#     max_tokens = 600,
+#     max_output_tokens: int = 400,
+# ) -> typing.Dict:
+#     batch = [record["prompt"]] + record['general_prompt']
+#     DS, DG_otherAQ = test_safety_gen(model, tok, batch, device, max_tokens, max_output_tokens)
+#     ret = {
+#         "DS": DS,
+#         "DG_otherAQ": DG_otherAQ
+#     }
+#     return ret
