60cc6e9f1f3302275d7d15c339ed02325f6e4cf2
JizhanFang <157343825+JizhanFang@users.noreply.github.com>
2025-03-06 20:08:55 +0800

    Update run_LLM_evaluation.py

--- examples/run_LLM_evaluation.py
+++ examples/run_LLM_evaluation.py
@@ -2,18 +2,22 @@ import os.path
 import sys
 import json
 import argparse
-
 sys.path.append('..')
 from easyeditor import (
-    FTHyperParams,
-    GraceHyperParams,
-    MEMITHyperParams,
-    ROMEHyperParams,
+    FTHyperParams, 
+    IKEHyperParams, 
+    KNHyperParams, 
+    MEMITHyperParams, 
+    ROMEHyperParams, 
+    LoRAHyperParams,
     MENDHyperParams,
-    WISEHyperParams,
-    BaseEditor,
-    summary_metrics,
-)
+    SERACHparams,
+    AlphaEditHyperParams
+    )
+from easyeditor import BaseEditor
+from easyeditor.models.ike import encode_ike_facts
+from sentence_transformers import SentenceTransformer
+from easyeditor import KnowEditDataset
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -21,51 +25,200 @@ if __name__ == "__main__":
     parser.add_argument('--hparams_dir', required=True, type=str)
     parser.add_argument('--data_dir', required=True, type=str)
     parser.add_argument('--output_dir', default='./outputs', type=str)
-    parser.add_argument('--ds_size', default=10, type=int)
+    parser.add_argument('--ds_size', default=None, type=int)
+    parser.add_argument('--datatype', default=None,type=str)
     parser.add_argument('--sequential_edit', default=False, type=bool)
-    parser.add_argument('--evaluation_type', default='real-world', type=str)
+    parser.add_argument('--train_data_path', type=str)
+    parser.add_argument('--evaluation_type', default='LLM-judge', type=str)
     parser.add_argument('--api_key', default=None, type=str)
 
     args = parser.parse_args()
 
     if args.editing_method == 'FT':
         editing_hparams = FTHyperParams
+    elif args.editing_method == 'IKE':
+        editing_hparams = IKEHyperParams
+    elif args.editing_method == 'ICE':
+        editing_hparams = IKEHyperParams
+    elif args.editing_method == 'KN':
+        editing_hparams = KNHyperParams
     elif args.editing_method == 'MEMIT':
         editing_hparams = MEMITHyperParams
     elif args.editing_method == 'ROME':
         editing_hparams = ROMEHyperParams
+    elif args.editing_method == 'LoRA':
+        editing_hparams = LoRAHyperParams
+    elif args.editing_method == 'SERAC':
+        editing_hparams = SERACHparams
     elif args.editing_method == 'MEND':
         editing_hparams = MENDHyperParams
-    elif args.editing_method == 'GRACE':
-        editing_hparams = GraceHyperParams
-    elif args.editing_method == 'WISE':
-        editing_hparams = WISEHyperParams
+    elif args.editing_method == 'AlphaEdit':
+        editing_hparams = AlphaEditHyperParams
     else:
         raise NotImplementedError
 
-    K = args.ds_size
-    # default using zsre
-    edit_data = json.load(open(os.path.join(args.data_dir, 'zsre_mend_edit.json'), 'r', encoding='utf-8'))[:K]
-    loc_data = json.load(open(os.path.join(args.data_dir, 'zsre_mend_train.json'), 'r', encoding='utf-8'))[:K]
+    datas = KnowEditDataset(args.data_dir,size=args.ds_size)
+    if args.datatype == 'counterfact' or args.datatype == 'recent' or args.datatype == 'zsre':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[data['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        
+        portability_r =[data['portability_r'] for data in datas]
+        portability_s =[data['portability_s'] for data in datas]
+        portability_l =[data['portability_l'] for data in datas]
 
-    loc_prompts = [edit_data_['loc'] + ' ' + edit_data_['loc_ans'] for edit_data_ in loc_data]
-    prompts = [edit_data_['src'] for edit_data_ in edit_data]
-    subject = [edit_data_['subject'] for edit_data_ in edit_data]
-    rephrase_prompts = [edit_data_['rephrase'] for edit_data_ in edit_data]
-    target_new = [edit_data_['alt'] for edit_data_ in edit_data]
-    locality_prompts = [edit_data_['loc'] for edit_data_ in edit_data]
-    locality_ans = [edit_data_['loc_ans'] for edit_data_ in edit_data]
-    locality_inputs = {
-        'neighborhood':{
-            'prompt': locality_prompts,
-            'ground_truth': locality_ans
-        },
-    }
-
-    hparams = editing_hparams.from_hparams(f'{args.hparams_dir}')
-    # specify real-world evaluation and provide the api key for LLM-as-a-Judge
-    hparams.evaluation_type = args.evaluation_type
+        portability_reasoning_prompts=[]
+        portability_reasoning_ans=[]
+        portability_Logical_Generalization_prompts=[]
+        portability_Logical_Generalization_ans=[]
+        portability_Subject_Aliasing_prompts=[]
+        portability_Subject_Aliasing_ans=[]
+        
+        portability_data = [portability_r,portability_s,portability_l]
+        portability_prompts = [portability_reasoning_prompts,portability_Subject_Aliasing_prompts,portability_Logical_Generalization_prompts]
+        portability_answers = [portability_reasoning_ans,portability_Subject_Aliasing_ans,portability_Logical_Generalization_ans]
+        for data, portable_prompts, portable_answers in zip(portability_data,portability_prompts,portability_answers):
+            for item in data:
+                if item is None:
+                    portable_prompts.append(None)
+                    portable_answers.append(None)
+                else:
+                    temp_prompts = []
+                    temp_answers = []
+                    for pr in item:
+                        prompt=pr["prompt"]
+                        an=pr["ground_truth"]
+                        while isinstance(an,list):
+                            an = an[0]
+                        if an.strip() =="":
+                            continue
+                        temp_prompts.append(prompt)
+                        temp_answers.append(an)
+                    portable_prompts.append(temp_prompts)
+                    portable_answers.append(temp_answers)
+        assert len(prompts) == len(portability_reasoning_prompts) == len(portability_Logical_Generalization_prompts) == len(portability_Subject_Aliasing_prompts)
+        
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        locality_Forgetfulness_prompts=[]        
+        locality_Forgetfulness_ans=[]
+        
+        locality_data = [locality_rs, locality_f]
+        locality_prompts = [locality_Relation_Specificity_prompts,locality_Forgetfulness_prompts]
+        locality_answers = [locality_Relation_Specificity_ans,locality_Forgetfulness_ans]
+        for data, local_prompts, local_answers in zip(locality_data,locality_prompts,locality_answers):
+            for item in data:
+                if item is None:
+                    local_prompts.append(None)
+                    local_answers.append(None)
+                else:
+                    temp_prompts = []
+                    temp_answers = []
+                    for pr in item:
+                        prompt=pr["prompt"]
+                        an=pr["ground_truth"]
+                        while isinstance(an,list):
+                            an = an[0]
+                        if an.strip() =="":
+                            continue
+                        temp_prompts.append(prompt)
+                        temp_answers.append(an)
+                    local_prompts.append(temp_prompts)
+                    local_answers.append(temp_answers)
+        assert len(prompts) == len(locality_Relation_Specificity_prompts) == len(locality_Forgetfulness_prompts)
+        locality_inputs = {}
+        portability_inputs = {}
+        
+        locality_inputs = {
+            'Relation_Specificity':{
+                'prompt': locality_Relation_Specificity_prompts,
+                'ground_truth': locality_Relation_Specificity_ans
+            },
+            'Forgetfulness':{
+                'prompt':locality_Forgetfulness_prompts,
+                'ground_truth':locality_Forgetfulness_ans
+            }
+        }
+        portability_inputs = {
+            'Subject_Aliasing':{
+                'prompt': portability_Subject_Aliasing_prompts,
+                'ground_truth': portability_Subject_Aliasing_ans
+            },
+            'reasoning':{
+                'prompt': portability_reasoning_prompts,
+                'ground_truth': portability_reasoning_ans           
+            },
+            'Logical_Generalization':{
+                'prompt': portability_Logical_Generalization_prompts,
+                'ground_truth': portability_Logical_Generalization_ans           
+            }
+        }
+    if args.datatype == 'wikibio':
+        prompts=[data['prompt'] for data in datas]
+        subjects=[data['subject'] for data in datas]
+        target_new = [data['target_new'] for data in datas]
+        
+        locality_rs = [data['locality_rs'] for data in datas]
+        locality_f = [data['locality_f'] for data in datas]
+        locality_Relation_Specificity_prompts=[]
+        locality_Relation_Specificity_ans=[]
+        
+        locality_data = [locality_rs]
+        locality_prompts = [locality_Relation_Specificity_prompts]
+        locality_answers = [locality_Relation_Specificity_ans]
+        for data, local_prompts, local_answers in zip(locality_data,locality_prompts,locality_answers):
+            for item in data:
+                if item is None:
+                    local_prompts.append(None)
+                    local_answers.append(None)
+                else:
+                    temp_prompts = []
+                    temp_answers = []
+                    for pr in item:
+                        prompt=pr["prompt"]
+                        an=pr["ground_truth"]
+                        while isinstance(an,list):
+                            an = an[0]
+                        if an.strip() =="":
+                            continue
+                        temp_prompts.append(prompt)
+                        temp_answers.append(an)
+                    local_prompts.append(temp_prompts)
+                    local_answers.append(temp_answers)
+        assert len(prompts) == len(locality_Relation_Specificity_prompts)
+        portability_inputs = None
+        locality_inputs = {}
+        locality_inputs = {
+            'Relation_Specificity':{
+                'prompt': locality_Relation_Specificity_prompts,
+                'ground_truth': locality_Relation_Specificity_ans
+            }
+        }
+    hparams = editing_hparams.from_hparams(args.hparams_dir)
     hparams.api_key = args.api_key
+    if args.editing_method == 'IKE':
+        train_ds = KnowEditDataset(args.train_data_path)
+        sentence_model = SentenceTransformer(hparams.sentence_model_name).to(f'cuda:{hparams.device}')
+        encode_ike_facts(sentence_model, train_ds, hparams)
+    elif args.editing_method == 'ICE':
+        hparams.use_icl_examples = False
+        train_ds = None
+    else:
+        train_ds = None
+    editor = BaseEditor.from_hparams(hparams)
+    
+    metrics, edited_model, _ = editor.edit(
+        prompts=prompts,
+        target_new=target_new,
+        subject=subjects,
+        locality_inputs=locality_inputs,
+        portability_inputs=portability_inputs,
+        train_ds=train_ds,
+        keep_original_weight=True,
+        test_generation=True,
+    )
 
     os.makedirs(args.output_dir, exist_ok=True)
     output_file = os.path.join(
@@ -75,19 +228,5 @@ if __name__ == "__main__":
 
     print("See results at: ", output_file)
 
-    editor = BaseEditor.from_hparams(hparams)
-    metrics, edited_model, _ = editor.edit(
-        prompts=prompts,
-        rephrase_prompts=rephrase_prompts,
-        target_new=target_new,
-        loc_prompts=loc_prompts,
-        subject=subject,
-        locality_inputs=locality_inputs,
-        sequential_edit=args.sequential_edit,
-    )
-
     with open(output_file, 'w') as f:
         json.dump(metrics, f, indent=4)
-
-    if len(metrics) > 0:
-        summary_metrics(metrics)
