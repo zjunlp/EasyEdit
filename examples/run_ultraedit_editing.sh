fcf873f0e59f61a14a17264aef71c0d9890847e7
HamJewel <1923398243@qq.com>
2025-06-06 21:03:16 +0800

    2025-6-6 21:03

--- examples/run_ultraedit_editing.sh
+++ examples/run_ultraedit_editing.sh
@@ -1,6 +1,6 @@
 python run_ultraedit_editing.py \
     --editing_method=UltraEdit \
-    --hparams_dir=../hparams/ULTRAEDIT/llama3-8b.yaml \
+    --hparams_dir=../hparams/ULTRAEDIT/mistral-7b.yaml \
     --data_dir=../data/ultraedit \
     --ds_size=20000 \
     --batch_size=100 \
