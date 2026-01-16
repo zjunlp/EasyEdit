21654ad21bc43c2a7bbdff8c86f8ff0969cf0e8c
asnbby <1143318515@qq.com>
2026-01-16 22:10:04 +0800

    新文件：   hparams/Steer/acl_experiment/apply_config.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/axbench_config.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_local_weight.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_lora.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_vector.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/apply_reps.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_local_weight.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_lora.yaml 	新文件：   hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_vector.yaml 	新文件：   hparams/Steer/acl_experiment/config.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/caa/apply_caa.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/caa/generate_caa.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_lora.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_vector.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_weight.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/apply_reps.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_local_weight.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_lora.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_vector.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/apply_sft.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_local_weight.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_lora.yaml 	新文件：   hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_vector.yaml 	新文件：   steer/trainer/SFTModelTrainer.py 	新文件：   steer/trainer/SFTTrainer.py 	新文件：   steer/vector_appliers/reps/apply_reps_hparam.py 	新文件：   steer/vector_appliers/sft/__init__.py 	新文件：   steer/vector_appliers/sft/apply_sft.py 	新文件：   steer/vector_appliers/sft/apply_sft_hparam.py 	新文件：   steer/vector_generators/reps/generate_reps.py 	新文件：   steer/vector_generators/reps/generate_reps_hparams.py 	新文件：   steer/vector_generators/sft/__init__.py 	新文件：   steer/vector_generators/sft/generate_sft.py 	新文件：   steer/vector_generators/sft/generate_sft_hparams.py 	新文件：   steer/vector_generators/sft/utils.py

---
+++ steer/trainer/SFTTrainer.py
@@ -0,0 +1,69 @@
+
+from .SFTModelTrainer import SFTModelTrainer
+import torch
+from tqdm.auto import tqdm
+
+class SFTTrainer(SFTModelTrainer):
+
+    # the base class for all preference models
+    preference_pairs = ["orig_add"] # "orig_add", "orig_sub", "steered_add", "steered_sub"
+    def __str__(self):
+        return 'SFTTrainer'
+
+    def make_model(self, **kwargs):
+        """
+        create a model with intervention
+        """
+        
+        from ..models.interventions import LocalWeightIntervention
+        from ..models.interventions import LoraIntervention
+        from ..models.interventions import VectorIntervention
+        
+        intervention_method = kwargs.get("intervention_method", "vector")
+        intervention_type = kwargs.get("intervention_type", "addition") # addition
+        if intervention_type == "addition":
+            if intervention_method == "vector":
+            # create a preference vector intervention object
+                steer_vector = VectorIntervention(
+                    embed_dim=kwargs.get("embed_dim", self.model.model.config.hidden_size), # set the embedding dimension
+                    low_rank_dimension=kwargs.get("low_rank_dimension", 1),            # set the low rank dimension, 4
+                    dropout=kwargs.get("dropout", 0.0),
+                    intervention_positions_dropout=kwargs.get("intervention_positions_dropout", 0.0) # set the dropout rate of the intervention positions
+                )
+            elif intervention_method == "local_weight":
+                steer_vector = LocalWeightIntervention(
+                    input_dim=kwargs.get("input_dim", self.model.model.config.hidden_size), # set the input dimension
+                    embed_dim=kwargs.get("embed_dim", self.model.model.config.hidden_size), # set the embedding dimension
+                    low_rank_dimension=kwargs.get("low_rank_dimension", 1),            # set the low rank dimension, 4
+                    dropout=kwargs.get("dropout", 0.0),
+                    intervention_components=kwargs.get("intervention_components", "mlp"),                                # set the dropout rate
+                    intervention_positions_dropout=kwargs.get("intervention_positions_dropout", 0.0) # set the dropout rate of the intervention positions
+                )
+            elif intervention_method == "lora":
+                steer_vector = LoraIntervention(
+                    input_dim=kwargs.get("input_dim", self.model.model.config.hidden_size), # set the input dimension
+                    embed_dim=kwargs.get("embed_dim", self.model.model.config.hidden_size), # set the embedding dimension
+                    low_rank_dimension=kwargs.get("low_rank_dimension", 1),            # set the low rank dimension, 4
+                    dropout=kwargs.get("dropout", 0.0),
+                    intervention_components=kwargs.get("intervention_components", "mlp"),                                # set the dropout rate
+                    intervention_positions_dropout=kwargs.get("intervention_positions_dropout", 0.0) # set the dropout rate of the intervention positions
+                )
+            else:
+                raise NotImplementedError(f"Intervention method {intervention_method} not implemented")
+        else:
+            raise ValueError(f"Intervention type {intervention_type} not supported")
+
+        self.intervention_type = intervention_type
+        self.model.steer_vector = steer_vector.to(self.model.device, dtype=self.model.torch_dtype)
+        self.model.steer_vector.train()
+        
+        self.preference_pairs = kwargs.get("preference_pairs", ["orig_add"])
+        
+        # set the model to eval mode and freeze the parameters
+        self.model.model.eval()
+        for param in self.model.model.parameters():
+            param.requires_grad = False
+        
+        for layer in self.layers:
+            intervention_copy = self.model.steer_vector  # all layers share the same intervention instance
+            self.model.set_intervention(layer, intervention_copy, "sft")
