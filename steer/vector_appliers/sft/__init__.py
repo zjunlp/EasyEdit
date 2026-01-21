31ef92715ea5a6283a88a83dacdb5832bf3a6a8e
asnbby <1143318515@qq.com>
2026-01-21 19:51:59 +0800

    Revert "	新文件：   hparams/Steer/acl_experiment/apply_config.yaml"
    This reverts commit 21654ad21bc43c2a7bbdff8c86f8ff0969cf0e8c.
    
    	删除：     axbench_experiment.py
    	删除：     hparams/Steer/acl_experiment/apply_config.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/axbench_config.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_local_weight.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_lora.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/our/generate_our_vector.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/apply_reps.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_local_weight.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_lora.yaml
    	删除：     hparams/Steer/acl_experiment/axbench/gemma-2-9b-it/reps/generate_reps_vector.yaml
    	删除：     hparams/Steer/acl_experiment/config.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/caa/apply_caa.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/caa/generate_caa.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_lora.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_vector.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/our/generate_our_weight.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/apply_reps.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_local_weight.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_lora.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/reps/generate_reps_vector.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/apply_sft.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_local_weight.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_lora.yaml
    	删除：     hparams/Steer/acl_experiment/psychopathy/gemma-2-9b-it/sft/generate_sft_vector.yaml
    	删除：     steer/trainer/SFTModelTrainer.py
    	删除：     steer/trainer/SFTTrainer.py
    	删除：     steer/vector_appliers/reps/apply_reps_hparam.py
    	删除：     steer/vector_appliers/sft/__init__.py
    	删除：     steer/vector_appliers/sft/apply_sft.py
    	删除：     steer/vector_appliers/sft/apply_sft_hparam.py
    	删除：     steer/vector_generators/reps/generate_reps.py
    	删除：     steer/vector_generators/reps/generate_reps_hparams.py
    	删除：     steer/vector_generators/sft/__init__.py
    	删除：     steer/vector_generators/sft/generate_sft.py
    	删除：     steer/vector_generators/sft/generate_sft_hparams.py
    	删除：     steer/vector_generators/sft/utils.py

--- steer/vector_appliers/sft/__init__.py
+++
@@ -1,4 +0,0 @@
-from .apply_sft import apply_sft, reset_sft_layers
-from .apply_sft_hparam import ApplySFTHyperParams
-
-__all__ = ['apply_sft', 'reset_sft_layers', 'ApplySFTHyperParams']
--- steer/vector_generators/sft/__init__.py
+++
@@ -1,3 +0,0 @@
-from .generate_sft_hparams import *
-from .generate_sft import *
-from .utils import *
